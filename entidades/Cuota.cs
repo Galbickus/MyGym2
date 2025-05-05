cc58768ce92fff83086bf3b5334b146477f24ebb
Veronica <vgalbicka@gmail.com>
2025-05-05 16:27:40 -0300

    add class Cuota

---
+++ entidades/Cuota.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyGym2.entidades
+{
+    internal class Cuota
+    {
+        public int id { get; }
+        public double ValorCouta { get; set; }
+        public DateTime FechaPago { get; set; }
+        public DateTime FechaVencimiento { get; set; }
+        public DateTime FechaProxVencimiento { get; set; }
+        public string FormaPago { get; set; }
+        public int CantidadCuotas { get; set; }
+        public bool Estado { get; set; }
+
+
+        public Cuota(int id, double valorCouta, DateTime fechaPago, DateTime fechaVencimiento, DateTime fechaProxVec,
+            string formaPago, int cantidadCuotas, bool estado)
+        {
+            this.id = id;
+            this.ValorCouta = valorCouta;
+            this.FechaPago = fechaPago;
+            this.FechaVencimiento = fechaVencimiento;
+            this.FechaProxVencimiento = fechaProxVec;
+            this.FormaPago = formaPago;
+            this.CantidadCuotas = cantidadCuotas;
+            this.Estado = estado;
+        }
+
+        public Cuota(double valorCouta, DateTime fechaPago, DateTime fechaVencimiento, DateTime fechaProxVec,
+            string formaPago, int cantidadCuotas, bool estado)
+        {
+            this.ValorCouta = valorCouta;
+            this.FechaPago = fechaPago;
+            this.FechaVencimiento = fechaVencimiento;
+            this.FechaProxVencimiento = fechaProxVec;
+            this.FormaPago = formaPago;
+            this.CantidadCuotas = cantidadCuotas;
+            this.Estado = estado;
+
+        }
+    }
+}
