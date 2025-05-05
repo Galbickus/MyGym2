4a93ff88f68f1f727ee119fc1bbf3215b3969d9f
Veronica <vgalbicka@gmail.com>
2025-05-05 16:27:00 -0300

    Add Usuario

---
+++ entidades/Usuario.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyGym2.entidades
+{
+    internal class Usuario : Persona
+    {
+        public int IdUsuario { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Rol { get; set; }
+
+        public Usuario(int id, string nombre, string apellido, string dni, string email,
+            string telefono, string username, string rol)
+            : base(nombre, apellido, dni, email, telefono)
+        {
+            IdUsuario = id;
+            Username = username;
+            Rol = rol;
+        }
+
+        public Usuario(string nombre, string apellido, string dni, string email,
+            string telefono, string username, string password, string rol)
+            : base(nombre, apellido, dni, email, telefono)
+        {
+            Username = username;
+            this.Password = password;
+            Rol = rol;
+        }
+
+        public Usuario(string nombre, string apellido, string dni, string username, string rol)
+           : base(nombre, apellido, dni)
+        {
+            Username = username;
+            Rol = rol;
+        }
+
+    }
+}
