21751940d0140815030c634a7305bafdf13c296f
Veronica <vgalbicka@gmail.com>
2025-05-07 18:24:13 -0300

    Add conexion

---
+++ Datos/Conexion.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace MyGym2.Datos
+{
+    internal class Conexion
+    {
+    }
+}
