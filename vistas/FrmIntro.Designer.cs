c72a2d907481267b1a188107d5bee87ac4a0867e
Veronica <vgalbicka@gmail.com>
2025-05-14 03:09:39 -0300

    Agrega pantalla de garga

---
+++ vistas/FrmIntro.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace MyGym2.vistas
+{
+    partial class FrmIntro
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmIntro));
+            this.label1 = new System.Windows.Forms.Label();
+            this.picLogo = new System.Windows.Forms.PictureBox();
+            this.pnlCarga = new System.Windows.Forms.Panel();
+            this.pnlCarga2 = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.picLogo)).BeginInit();
+            this.pnlCarga.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(146, 163);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(266, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Sistema Gestor de Gimnasio";
+            // 
+            // picLogo
+            // 
+            this.picLogo.Image = ((System.Drawing.Image)(resources.GetObject("picLogo.Image")));
+            this.picLogo.Location = new System.Drawing.Point(214, 52);
+            this.picLogo.Name = "picLogo";
+            this.picLogo.Size = new System.Drawing.Size(80, 80);
+            this.picLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picLogo.TabIndex = 1;
+            this.picLogo.TabStop = false;
+            // 
+            // pnlCarga
+            // 
+            this.pnlCarga.Controls.Add(this.pnlCarga2);
+            this.pnlCarga.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.pnlCarga.Location = new System.Drawing.Point(0, 280);
+            this.pnlCarga.Name = "pnlCarga";
+            this.pnlCarga.Size = new System.Drawing.Size(525, 20);
+            this.pnlCarga.TabIndex = 2;
+            // 
+            // pnlCarga2
+            // 
+            this.pnlCarga2.BackColor = System.Drawing.Color.White;
+            this.pnlCarga2.Location = new System.Drawing.Point(0, 0);
+            this.pnlCarga2.Name = "pnlCarga2";
+            this.pnlCarga2.Size = new System.Drawing.Size(37, 20);
+            this.pnlCarga2.TabIndex = 0;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 20;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // FrmIntro
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.ClientSize = new System.Drawing.Size(525, 300);
+            this.Controls.Add(this.pnlCarga);
+            this.Controls.Add(this.picLogo);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Roboto", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.White;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "FrmIntro";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FrmIntro";
+            ((System.ComponentModel.ISupportInitialize)(this.picLogo)).EndInit();
+            this.pnlCarga.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox picLogo;
+        private System.Windows.Forms.Panel pnlCarga;
+        private System.Windows.Forms.Panel pnlCarga2;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
---
+++ vistas/FrmIntro.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyGym2.vistas
+{
+    public partial class FrmIntro : Form
+    {
+        public FrmIntro()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            pnlCarga2.Width += 9;
+            if (pnlCarga2.Width >= 525)
+            {
+                timer1.Stop();
+                LoginForm login = new LoginForm();
+                login.Show();
+                this.Hide();
+            }
+        }
+    }
+}
